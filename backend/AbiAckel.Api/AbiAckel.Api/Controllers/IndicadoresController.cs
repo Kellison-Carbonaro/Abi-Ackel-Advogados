5b5eacdcba64488228af3bf9c3397998418da3bc
kellison <kellisonvinicius@gmail.com>
2026-01-06 21:06:53 -0300

    projeto inicial front e backend finalizado

---
+++ backend/AbiAckel.Api/AbiAckel.Api/Controllers/IndicadoresController.cs
@@ -0,0 +1,25 @@
+﻿using AbiAckel.Api.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AbiAckel.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class IndicadoresController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IndicadoresDto> Get()
+        {
+            var indicadores = new IndicadoresDto
+            {
+                ProcessosAtivos = 120,
+                ProcessosGanhos = 35,
+                ProcessosPerdidos = 10,
+                PrazoMedioDias = 18,
+                AudienciasSemana = 5
+            };
+
+            return Ok(indicadores);
+        }
+    }
+}
---
+++ backend/AbiAckel.Api/AbiAckel.Api/DTOs/IndicadoresDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AbiAckel.Api.DTOs
+{
+    public class IndicadoresDto
+    {
+        public int ProcessosAtivos { get; set; }
+        public int ProcessosGanhos { get; set; }
+        public int ProcessosPerdidos { get; set; }
+        public int PrazoMedioDias { get; set; }
+        public int AudienciasSemana { get; set; }
+    }
+}
